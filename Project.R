d08dd5346cdfbc23aae7fb02f81dc35401b9698f
Julianne Buggs <jbugg005@fiu.edu>
2024-04-11 19:28:40 -0400

    Initial draft

---
+++ Project.R
@@ -0,0 +1,8 @@
+####Independent Project
+
+###Zooplankton Abundance in the Bering Sea
+###NOAA NCEI Ascension Number 0211098
+
+##PROBABLY 2-Way ANOVA
+
+plankton <-read.table(file = "clipboard", sep = "\t", header = TRUE)
